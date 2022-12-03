a80098b8b1004e98ed57e6076b111c8b30ae80c2
Falme Streamless <falme_streamless@hotmail.com>
2022-12-03 04:21:24 -0300

    Created BAT file to auto-generate unity folder structure

---
+++ FolderStructure.bat
@@ -0,0 +1,14 @@
+mkdir Animations
+mkdir Editor
+mkdir Fonts
+mkdir Materials
+mkdir Prefabs
+mkdir Resources
+mkdir Scenes
+mkdir Scripts
+mkdir Sounds
+mkdir Extensions
+mkdir Textures
+cd Textures
+mkdir UI
+mkdir Icons
